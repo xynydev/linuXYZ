00325afb510c9c18b7d280d671c93a405140a866
xynydev <eino.rauhala@proton.me>
2023-10-28 17:33:25 +0300

    chore: chmod binary, dl service to correct dir

--- config/scripts/rescrobbled.sh
+++ config/scripts/rescrobbled.sh
@@ -5,9 +5,10 @@ echo "Downloading latest rescrobbled binary"
 curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/InputUsername/rescrobbled/releases/latest \
     | jq .assets[0].browser_download_url \
     | xargs wget -o /usr/bin/rescrobbled
+chmod +x /usr/bin/rescrobbled
 
 echo "Downloading rescrobbled systemd unit"
-curl https://raw.githubusercontent.com/InputUsername/rescrobbled/master/rescrobbled.service > Downloads/rescrobbled.service
+curl https://raw.githubusercontent.com/InputUsername/rescrobbled/master/rescrobbled.service > /usr/lib/systemd/user/rescrobbled.service
 
 echo "Enabling rescrobbled systemd unit"
 systemctl enable --global -f rescrobbled.service
