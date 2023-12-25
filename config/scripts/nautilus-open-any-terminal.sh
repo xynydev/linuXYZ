53ed9167bba1213256a475046f638eb576a9edae
xynydev <eino.rauhala@proton.me>
2023-12-25 18:02:36 +0200

    fix: try different type of nauttermext install?

--- config/scripts/nautilus-open-any-terminal.sh
+++ config/scripts/nautilus-open-any-terminal.sh
@@ -6,7 +6,10 @@ echo "Installing nautilus-python"
 rpm-ostree install nautilus-python
 
 echo "Installing nautilus-open-any-terminal"
-pip install nautilus-open-any-terminal
+cd /tmp/
+git clone https://github.com/Stunkymonkey/nautilus-open-any-terminal.git
+cd nautilus-open-any-terminal
+./tools/update-extension-system.sh install
 
 echo "Setting default terminal to kitty"
 gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal kitty
