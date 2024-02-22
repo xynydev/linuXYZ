943d28a1ed3accd91ea6deb84bc6ae8170793901
xynydev <60004820+xynydev@users.noreply.github.com>
2024-02-22 13:32:57 +0200

    fix: use raw output flag with jq in ytmusic script

--- config/scripts/youtube-music-app.sh
+++ config/scripts/youtube-music-app.sh
@@ -3,10 +3,10 @@ set -xoue pipefail
 
 cd /tmp
 
-ASSETS_URL=$(curl https://api.github.com/repos/th-ch/youtube-music/releases/latest | jq .assets_url)
+ASSETS_URL=$(curl https://api.github.com/repos/th-ch/youtube-music/releases/latest | jq '.assets_url' --raw-output)
 
-LATEST_LINUX_YML_RELASE_URL=$(curl "$ASSETS_URL" | jq '.[0].url')
-LATEST_LINUX_YML_DOWNLOAD_URL=$(curl "$LATEST_LINUX_YML_RELASE_URL" | jq '.browser_download_url')
+LATEST_LINUX_YML_RELASE_URL=$(curl "$ASSETS_URL" | jq '.[0].url' --raw-output)
+LATEST_LINUX_YML_DOWNLOAD_URL=$(curl "$LATEST_LINUX_YML_RELASE_URL" | jq '.browser_download_url' --raw-output)
 wget "$LATEST_LINUX_YML_DOWNLOAD_URL"
 
 RPM_FILENAME=$(yq '.files[2].url' latest-linux.yml)
