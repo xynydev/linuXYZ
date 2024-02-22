fef2edd920b95bd75846a68d6fb2b2f7a2c7beb1
xynydev <60004820+xynydev@users.noreply.github.com>
2024-02-22 12:50:55 +0200

    feat: install youtube music app

---
+++ config/scripts/youtube-music-app.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -oue pipefail
+
+cd /tmp
+
+ASSETS_URL=$(curl https://api.github.com/repos/th-ch/youtube-music/releases/latest | jq .assets_url)
+
+LATEST_LINUX_YML_RELASE_URL=$(curl "$ASSETS_URL" | jq '.[0].url')
+LATEST_LINUX_YML_DOWNLOAD_URL=$(curl "$LATEST_LINUX_YML_RELASE_URL" | jq '.browser_download_url')
+wget "$LATEST_LINUX_YML_DOWNLOAD_URL"
+
+RPM_FILENAME=$(yq '.files[2].url' latest-linux.yml)
+VERSION_TAG="v$(yq '.version' latest-linux.yml)"
+RPM_DOWNLOAD_URL="https://github.com/th-ch/youtube-music/releases/download/$VERSION_TAG/$RPM_FILENAME"
+
+rpm-ostree install "$RPM_DOWNLOAD_URL"
