eb357d6d5fa4e8a9ba553aba3b37e6895a6bec7d
xynydev <60004820+xynydev@users.noreply.github.com>
2024-03-18 19:53:23 +0200

    fix: don't try to install figma direct from url

---
+++ config/scripts/figma.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -oue pipefail
+
+cd /tmp
+curl https://github.com/Figma-Linux/figma-linux/releases/download/v0.11.3/figma-linux_0.11.3_linux_x86_64.rpm > figma.rpm
+rpm-ostree install ./figma.rpm
