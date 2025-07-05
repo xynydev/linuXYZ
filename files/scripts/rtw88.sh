23d69905c1609490f0eb38a6767d23d9cdd2ca94
xyny <git@xyny.anonaddy.me>
2025-07-05 22:27:04 +0300

    fix: try another install method

--- files/scripts/rtw88.sh
+++ files/scripts/rtw88.sh
@@ -2,15 +2,8 @@
 set -oue pipefail
 
 cd /tmp
-
 git clone https://github.com/lwfinger/rtw88
-
 cd rtw88
 
-make
-
-sudo make install
-
-sudo make install_fw
-
-sudo modprobe rtw_8814au
+dkms install $PWD
+make install_fw
