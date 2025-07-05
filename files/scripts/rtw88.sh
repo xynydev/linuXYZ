4a7d5e5dbe8bc70eaa3a5f9946d6fe6375a645ac
xyny <git@xyny.anonaddy.me>
2025-07-05 22:44:27 +0300

    fix: install kernel devel for bazzite

--- files/scripts/rtw88.sh
+++ files/scripts/rtw88.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
+cd /tmp/kernel-rpms
+dnf install -y kernel-devel*
+
 cd /tmp
 git clone https://github.com/lwfinger/rtw88
 cd rtw88
 
-dnf install -y kernel-devel kernel-headers dkms
+dnf install -y dkms
 
 dkms install $PWD
 make install_fw
